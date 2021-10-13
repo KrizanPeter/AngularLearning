ad68faaa4f7a5515dd5c09f156d526a14c065f25
Peter Krizan <krizan.peter.fiit@gmail.com>
2021-10-13 15:32:04 +0200

    featdeployment): vol3

--- BoardGame.Domain/Entities/AppUserRole.cs
+++ BoardGame.Domain/Entities/AppUserRole.cs
@@ -1,14 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BoardGame.Domain.Entities
 {
     public class AppUserRole : IdentityUserRole<int>
     {
         public virtual AppUser User { get; set; }
+
         public virtual AppRole Role { get; set; }
 
     }

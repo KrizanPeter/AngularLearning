ef78ebcb3f4dc3f427ddfee7f4cd5b270de0de88
PeterKrizan <krizan.peter.fiit@gmail.com>
2021-10-21 01:33:55 +0200

    deploy(migration): new migration

--- BoardGame.Domain/Migrations/20211020233330_InitialMigration.Designer.cs
+++ BoardGame.Domain/Migrations/20211020233330_InitialMigration.Designer.cs
@@ -10,7 +10,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BoardGame.Domain.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20211020173840_InitialMigration")]
+    [Migration("20211020233330_InitialMigration")]
     partial class InitialMigration
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
